1c438e428e158bc12656149702100fa9548e7b5c
Kolko Valentin <valiant9191@gmail.com>
2020-11-27 11:57:15 +0300

    comparee

--- src/components/News.js
+++ src/components/News.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import NewsBlock from './newsComponents/newsBlock';
-<<<<<<< HEAD
-=======
 import NewsBlock2 from './newsComponents/newsBlock2';
 import NewsBlock3 from './newsComponents/newsBlock3';
 import NewsBlock4 from './newsComponents/newsBlock4';
 import NewsBlock5 from './newsComponents/newsBlock5';
 import NewsBlock6 from './newsComponents/newsBlock6';
->>>>>>> d1b2f67b89a22b5a2ce27a808ac85d7d36980610
 
 import '../css/CryptoNews.css';
 
@@ -22,19 +19,11 @@ import '../css/CryptoNews.css';
 
         <div className='news'>
           <div className='newsBlock'> <NewsBlock /> </div> 
-<<<<<<< HEAD
-          <div className='newsBlock'> <NewsBlock /> </div> 
-          <div className='newsBlock'> <NewsBlock /> </div> 
-          <div className='newsBlock'> <NewsBlock /> </div> 
-          <div className='newsBlock'> <NewsBlock /> </div> 
-          <div className='newsBlock'> <NewsBlock /> </div>                  
-=======
           <div className='newsBlock'> <NewsBlock2 /> </div> 
           <div className='newsBlock'> <NewsBlock3 /> </div> 
           <div className='newsBlock'> <NewsBlock4 /> </div> 
           <div className='newsBlock'> <NewsBlock5 /> </div> 
           <div className='newsBlock'> <NewsBlock6 /> </div>                  
->>>>>>> d1b2f67b89a22b5a2ce27a808ac85d7d36980610
         </div>
 
       </div>
