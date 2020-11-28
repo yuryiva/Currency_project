fc21fa688cb540ae5322ac43531ddc97a18530c6
Kolko Valentin <valiant9191@gmail.com>
2020-11-28 22:49:28 +0300

    change date in news

--- src/components/newsComponents/fullUrl.js
+++ src/components/newsComponents/fullUrl.js
@@ -4,7 +4,7 @@
 const apiKey = "c5030163187e4eeeaedee411fe43f0e6";
 const newsApiOne =`http://newsapi.org/v2/everything?`;
 const filterCategory = 'bitcoin';
-const dateOfNews = '2020-10-27';//new Date(year, month, day);
+const dateOfNews = new Date();
 
 
 const fullUrl = `${newsApiOne}q=${filterCategory}&to=${dateOfNews}&sortBy=publishedAt&language
