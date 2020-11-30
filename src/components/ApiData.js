90805bf7338087a91206844a978c7917f4e6352f
dima <galtd@list.ru>
2020-11-30 20:38:01 +0300

    sdelal cliki

--- src/components/ApiData.js
+++ src/components/ApiData.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react';
+import React, { Component, useState } from 'react';
 import '../css/ApiData.css'
 import Coin from "./Coin"
 import Chart1 from './Chart1'
@@ -13,113 +13,132 @@ const apiUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&o
 
 class App extends Component {
 
-    state = {
-      apiLoaded: false,
-      data: {} ,
-      search: 'eth',
-      winner: false
-    }
+  state = {
+    apiLoaded: false,
+    data: {},
+    search: 'eth',
+    winner: false
+  }
 
-  
 
-     componentDidMount() {
 
-        fetch(apiUrl) 
-          .then(response => (response.json()))
-          .then(dataFromApi => (
-            this.setState({
-              data: dataFromApi,
-              apiLoaded: true 
-              
-            })
-            
-          ))
-      }
+  componentDidMount() {
 
-     handleChange = (event) => 
-      this.setState({
-        search: event.target.value
+    fetch(apiUrl)
+      .then(response => (response.json()))
+      .then(dataFromApi => {
+
+        let dataWithNewProperties = dataFromApi.map(coin => ({...coin, show:false}))
+        
+
+        this.setState({
+          data: dataWithNewProperties,
+          apiLoaded: true
+        })
+        
       })
-    
-     
+  }
+
+  handleChange = (event) =>
+    this.setState({
+      search: event.target.value
+    })
 
 
-     filteredCoins = () => {
-      let filterCoin = this.state.data.filter((coin)=>
-       coin.id.toLowerCase().includes(this.state.search.toLowerCase()) || coin.symbol.toLowerCase().includes(this.state.search.toLowerCase()))
-       return filterCoin;
-     }
 
 
-     chooseCoin = () =>
-
-  this.setState({
-      winner: !this.state.winner
-  })
-
-
-      render() {
-        return (
-          <div className="coin-app">
-
-            {
-              this.state.apiLoaded
-                ?
+  filteredCoins = () => {
+    let filterCoin = this.state.data.filter((coin) =>
+      coin.id.toLowerCase().includes(this.state.search.toLowerCase()) || coin.symbol.toLowerCase().includes(this.state.search.toLowerCase()))
+    return filterCoin;
+  }
 
 
-                <div>
 
-                  <div className="coin-search">
-            <h1>Search a currency</h1>
-            <form>
-              <input 
-              type="text"
-               placeholder="search" 
-               className="coin-input"
-               onChange={this.handleChange}
-               />
-             </form>
-             </div>
-                   <div className="allCoins">
-                     
-                  {this.filteredCoins().map(character =>{
-                    return(
-                       <div>
-                      <div className={character.id} onClick={this.chooseCoin}>
-                      <Coin 
-                      id={character.id}
-                      image={character.image}
-                      symbol={character.symbol.toUpperCase()}
-                     />
-                     </div>
-                     
-                     <div className= {character.id}  style={{display: this.state.winner ? 'block' : 'none' }}   >
-                      <CoinMoreStuff
-                      price={character.current_price}
-                      volume={character.market_cap}
-                      priceChange={character.price_change_percentage_24h}
-                      />
+
+ 
+
+
+  chooseCoin = (index) => {
+    let arrayOfCoinsToShow = [...this.state.data]
+
+    if(arrayOfCoinsToShow[index].show){
+      arrayOfCoinsToShow[index].show = !this.state.data[index].show 
+    }else{
+    for (let i = 0; i<arrayOfCoinsToShow.length; i++){
+      arrayOfCoinsToShow[i].show = false;
+}
+arrayOfCoinsToShow[index].show = !this.state.data[index].show
+}
+
+
+    this.setState({
+        data: arrayOfCoinsToShow
+       })
+  }
+
+
+  render() {
+    return (
+      <div className="coin-app">
+
+        {
+          this.state.apiLoaded
+            ?
+
+
+            <div>
+
+              <div className="coin-search">
+                <h1>Search a currency</h1>
+                <form>
+                  <input
+                    type="text"
+                    placeholder="search"
+                    className="coin-input"
+                    onChange={this.handleChange}
+                  />
+                </form>
+              </div>
+              <div className="allCoins">
+
+                {this.filteredCoins().map((character, index) => {
+                  return (
+                    <div>
+                      <div className={character.id} onClick={() => this.chooseCoin(index)}>
+                        <Coin
+                          id={character.id}
+                          image={character.image}
+                          symbol={character.symbol.toUpperCase()}
+                        />
                       </div>
 
-                     </div>
+                      <div style={{ display: this.state.data[index].show ? 'block' : 'none' }}   >
+                        <CoinMoreStuff
+                          price={character.current_price}
+                          volume={character.market_cap}
+                          priceChange={character.price_change_percentage_24h}
+                        />
+                      </div>
 
-                    )
-                  })}
-                  
-                     <Chart1 />
-                  </div>
+                    </div>
 
-                </div>
-                : 
-                <p>SHOW SOME LOADING SPINNER</p>
-            }
-    
-          </div>
-        )
-      }
-    }
-    
-    
-    export default App;
+                  )
+                })}
 
+                <Chart1 />
+              </div>
+
+            </div>
+            :
+            <p>SHOW SOME LOADING SPINNER</p>
+        }
+
+      </div>
+    )
+  }
+}
+
+
+export default App;
 
