3a12188ce5052cf2d129e8442d6be20184740a1d
dima <galtd@list.ru>
2020-11-30 22:40:29 +0300

    ready to merge

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -28,7 +28,7 @@ class Coin extends Component {
     <p className="coin-volume">${this.props.volume.toLocaleString()}</p>
             {this.props.priceChange < 0 ? (
                 <p className="coin-percent red">{this.props.priceChange.toFixed(2)}%</p>
-            ) : <p className="coin-percent green">{this.props.priceChange.toFixed(2)}%</p>
+            ) : <p className="coin-percent green">{this.props.priceChange}%</p>
 
         
         } 
