09b0dcbebd0b76dbeb6d8077fecf3d69a0245914
Kolko Valentin <valiant9191@gmail.com>
2020-12-04 21:33:59 +0300

    add pages router

---
+++ src/components/Charts/chart-page.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import NewsBlock from '../newsComponents/newsBlock'
+
+class ChartPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello CHART</h1>
+            </div>
+        )
+    }
+}
+
+
+
+
+
+export default ChartPage;
