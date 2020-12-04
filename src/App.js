e173a0060ff2f9a2b2dfb97cdc926fb49fff8c2b
Kolko Valentin <valiant9191@gmail.com>
2020-12-04 21:40:02 +0300

    add components &Route with path to different pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom';
 import './App.css';
-import ApiData from './components/ApiData'
-import Navig from './components/Navig'
-import TradeWithUs from './components/TradeWithUs'
+import ApiData from './components/ApiData';
+import Navig from './components/Navig';
+import TradeWithUs from './components/TradeWithUs';
 import CryptoNews from './components/News';
-import Footer from './components/footer/Footer'
+import Footer from './components/footer/Footer';
+import AboutPage from "./components/About/about-page";
+import NewsPage from "./components/News/news-page";
+import ChartsPage from "./components/Charts/chart-page";
+
+
 
 
 function App() {
@@ -13,17 +19,18 @@ function App() {
       <div className = 'navMenu'>
         <Navig/>
       </div>
-      <div className = 'mainchart'>
-     
-      <ApiData/>
-      </div>
-      <TradeWithUs />
-  
+                  
+          <Switch>
+           <Route path="/About" component={AboutPage} />          
+           <Route path="/News" component={NewsPage} />
+           <Route path="/Charts" component={ChartsPage} />
+           <Route exact path="/" >
+              <ApiData />
+              <CryptoNews/> 
+              <TradeWithUs/>
+            </Route>
+         </Switch> 
 
-
-
-      {/* news block */}
-      <CryptoNews />
 
      <Footer />
 
