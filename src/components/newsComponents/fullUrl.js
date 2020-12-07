909d90193844cbe4f9c37163d8fe8c649139fad2
Kolko Valentin <valiant9191@gmail.com>
2020-12-07 14:30:03 +0300

    add instruction how toget api key in fullurl file

--- src/components/newsComponents/fullUrl.js
+++ src/components/newsComponents/fullUrl.js
@@ -1,4 +1,9 @@
-// url
+// instruction how to get an Api Key: create file .env in home directory of files(at the same directory with this files README.md, package.json etc)
+//      inside .env add REACT_APP_API_NEWS_KEY= 
+//      after equals write your API key
+//          how to get API Key: go to this link: http://newsapi.org/
+//          push button Get API Key  YOU CAN SEE api key   
+// 
 
 
 
