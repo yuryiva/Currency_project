c7281bb2447657eb9c0653e54cf740e50540d749
dima <galtd@list.ru>
2020-12-08 18:45:53 +0300

    e

--- src/components/CoinMoreStuff.js
+++ src/components/CoinMoreStuff.js
@@ -13,19 +13,20 @@ class CoinMoreStuff extends Component {
 
  render(){
     return(
+
         <>
 
-             { <div className="moreStufs">
-    <p className="coin-price">${this.props.price}</p>
-    <p className="coin-volume">${this.props.volume.toLocaleString()}</p>
-            {this.props.priceChange < 0 ? (
-                <p className="coin-percent red"><i className="fas fa-long-arrow-alt-down"></i>{this.props.priceChange.toFixed(2).replace(/-/, ' ')}%</p>
-            ) : <p className="coin-percent green"><i className="fas fa-long-arrow-alt-up"> </i>{this.props.priceChange.toFixed(2)}%</p>
+        { <div className="moreStufs">
+<p className="coin-price">${this.props.price}</p>
+<p className="coin-volume">${this.props.volume.toLocaleString()}</p>
+       {this.props.priceChange < 0 ? (
+           <p className="coin-percent red"><i className="fas fa-long-arrow-alt-down"></i>{this.props.priceChange.toFixed(2).replace(/-/, ' ')}%</p>
+       ) : <p className="coin-percent green"><i className="fas fa-long-arrow-alt-up"> </i>{this.props.priceChange.toFixed(2)}%</p>
 
-        
-        } 
-             </div> }
-        </>
+   
+   } 
+        </div> }
+   </>
     )}
 }
 
