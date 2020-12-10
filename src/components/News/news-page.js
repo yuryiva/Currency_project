db51a390c06bf830778ceabce86a98dd05a07e72
Kolko Valentin <valiant9191@gmail.com>
2020-12-10 20:14:04 +0300

    add new api<playing

--- src/components/News/news-page.js
+++ src/components/News/news-page.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import  './news-page.css'
 import NewsBlockApi from '../newsComponents/newsBlockApi'
-
+import ApiNewsNYT from './nytApi/ApiNewsNYT'
 
 class NewsPage extends Component {
 
--- src/components/News/nytApi/ApiNewsNYT.js
+++ src/components/News/nytApi/ApiNewsNYT.js
@@ -30,17 +30,18 @@ import '../news-page.css'
       <div>
 
      { this.state.newsData.title}<h1>hello</h1>
-{/* 
-        {this.state.getData ?          
+
+        {/* {this.state.getData ?          
         
         `${this.state.status==='error'? 
         <h1>Error</h1>
         :
-        this.state.newsData.map((result, index) => (<div>  
+        this.state.newsData.map((result, index) => {console.log(result.title);
+        return<div>  
           key={index}
           title={result.title}
           </div>
-          ))
+          })
          }`
 
 
@@ -53,3 +54,33 @@ import '../news-page.css'
 }
 
 export default ApiNewsNYT;
+
+
+//  {/* { this.state.newsData.title}<h1>hello</h1> */}
+//  { this.state.newsData ? this.state.newsData.map((el, i) =>
+//   <NewsWrap
+//   title={el.title}
+//   />)
+//   //Object.entries(data).map()
+// //   const NewsWrap = ({ title, updated_date, source}) => (
+// // <div>
+// //   <h2>{title}</h2>
+// //   <h2>{updated_date}</h2>
+// //   <p>{source}</p>
+// // </div>
+// //   )
+//   // `${this.state.status === 'error' ?
+//   //   <h1>Error</h1>
+//   //   :
+//   //   this.state.newsData.map((result, index) => (<div>
+//   //     key={index}
+//   // title={result.title}
+//   //   </div>
+//   //   ))
+//   // }`
+//   // console.log(this.state.newsData)
+//   : <p>Loading...</p>  
+// }
+// </div>
+// )
+// }`
