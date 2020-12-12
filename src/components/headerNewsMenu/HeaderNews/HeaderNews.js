33de0f48874344067c06af962d605f7e484a61c8
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2020-12-12 16:34:55 +0300

    search news by category added

--- src/components/headerNewsMenu/HeaderNews/HeaderNews.js
+++ src/components/headerNewsMenu/HeaderNews/HeaderNews.js
@@ -14,16 +14,34 @@ export default class HeaderNews extends Component {
     data: {},
     dataFromApiReceived: false,
     inputValue: "",
+    values: [
+      { name: "Bitcoin", id: 1 },
+      { name: "Dash", id: 2 },
+      { name: "IOTA", id: 3 },
+      { name: "NEO", id: 4 },
+      { name: "Litecoin", id: 5 },
+      { name: "Ripple", id: 6 },
+      { name: "Zcash", id: 7 },
+      { name: "EOS", id: 8 },
+      { name: "TRON", id: 9 },
+      { name: "Binance", id: 10 },
+      { name: "Bittrex", id: 11 },
+      { name: "Coinbase", id: 12 },
+      { name: "IDEX", id: 13 },
+      { name: "Poloniex", id: 14 },
+      { name: "Kraken", id: 15 },
+      { name: "Huobi", id: 16 },
+    ],
+
+    topicChosen: "",
   };
 
-  
-  newsByDefaultApi = `http://newsapi.org/v2/top-headlines?country=us&apiKey=8fbdc45f38704319840644466e6eaf15`
-  
-  // ${process.env.REACT_APP_API_NEWS_KEY}
+  newsByDefaultApi = `http://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_API_NEWS_KEY}`;
 
-  
-  newsByRequestApi = `http://newsapi.org/v2/everything?q=${this.state.inputValue}&from=${new Date}&sortBy=popularity&apiKey=8fbdc45f38704319840644466e6eaf15`
-  // ${process.env.REACT_APP_API_NEWS_KEY}
+  newsByRequestApi = `http://newsapi.org/v2/everything?q=${
+    this.state.inputValue
+  }&from=${new Date()}&sortBy=popularity&apiKey=8fbdc45f38704319840644466e6eaf15`;
+  // ${process.env.REACT_APP_API_NEWS_KEY}`
 
   componentDidMount() {
     fetch(this.newsByDefaultApi)
@@ -40,18 +58,20 @@ export default class HeaderNews extends Component {
     });
   }
 
-  handleChange = (event) => {
+  handleKeyWordChange = (event) => {
     this.setState({
       inputValue: event.target.value,
     });
   };
 
-  handleSubmit = (event) => {
+  handleKeyWordSubmit = (event) => {
     event.preventDefault();
     fetch(
-      `http://newsapi.org/v2/everything?q=${this.state.inputValue}&from=${new Date}&sortBy=popularity&apiKey=8fbdc45f38704319840644466e6eaf15`
-      
-      // ${process.env.REACT_APP_API_NEWS_KEY}
+      `http://newsapi.org/v2/everything?q=${
+        this.state.inputValue
+      }&from=${new Date()}&sortBy=popularity&apiKey=8fbdc45f38704319840644466e6eaf15`
+
+      // ${process.env.REACT_APP_API_NEWS_KEY}`
     )
       .then((response) => response.json())
       .then((dataFromApi) =>
@@ -66,37 +86,62 @@ export default class HeaderNews extends Component {
     });
   };
 
+  handleTopicChoise = (event) => {
+    this.setState({
+      topicChosen: event.target.value,
+    });
+
+    setTimeout(() => {
+      fetch(
+        `http://newsapi.org/v2/everything?q=${
+          this.state.topicChosen
+        }&from=${new Date()}&sortBy=popularity&apiKey=8fbdc45f38704319840644466e6eaf15`
+      )
+        .then((response) => response.json())
+        .then((dataFromApi) =>
+          this.setState({
+            data: dataFromApi.articles,
+            dataFromApiReceived: true,
+          })
+        );
+
+      this.setState({
+        dataFromApiReceived: false,
+      });
+    }, 1);
+  };
+
   render() {
     return (
       <div className="mainPart">
         {/* {console.log(this.state.data)} */}
         <SearchBlock
-          handleChange={this.handleChange}
-          handleSubmit={this.handleSubmit}
+          handleTopicChoise={this.handleTopicChoise}
+          topics={this.state.values.map((object) => (
+            <option key={object.id} value={object.name}>
+              {object.name}
+            </option>
+          ))}
+          handleKeyWordChange={this.handleKeyWordChange}
+          handleKeyWordSubmit={this.handleKeyWordSubmit}
           value={this.state.inputValue}
         />
 
         {this.state.dataFromApiReceived === true ? (
           this.state.data.map((object, index) => (
-            
-              <NewsBox
-              
-                key={index}
-                url={object.url}
-                urlToImage={object.urlToImage}
-                title={object.title}
-                author={object.author}
-                description={object.description}
-              />
-            
+            <NewsBox
+              key={index}
+              url={object.url}
+              urlToImage={object.urlToImage}
+              title={object.title}
+              author={object.author}
+              description={object.description}
+            />
           ))
         ) : (
           <h1>LOADING</h1>
         )}
       </div>
-      
     );
   }
-  
 }
-
--- src/components/headerNewsMenu/SearchBlock/SearchBlock.js
+++ src/components/headerNewsMenu/SearchBlock/SearchBlock.js
@@ -7,18 +7,26 @@ const SearchBlock = (props) => {
   // const [value, onChange] = useState(new Date());
   return (
     <div className="searchBlock">
-      <form onSubmit={(e) => props.handleSubmit(e)}>
+      <label className="label">
+        NEWS ON PARTICULAR TOPIC
+        <select onChange={(e) => props.handleTopicChoise(e)}>
+          {props.topics}
+        </select>
+      </label>
+
+      <form onSubmit={(e) => props.handleKeyWordSubmit(e)}>
         <label className="label">
           NEWS BY KEYWORD
           <input
             className="input"
             type="text"
             value={props.value}
-            onChange={(e) => props.handleChange(e)}
+            onChange={(e) => props.handleKeyWordChange(e)}
           />
         </label>
         <input className="searchButton" type="submit" value="SEARCH" />
       </form>
+
       {/* <input type="date" max="2020-12-10" min="2020-11-12" value="2020-12-10"/> */}
       {/* <Calendar
         onChange={onChange}
